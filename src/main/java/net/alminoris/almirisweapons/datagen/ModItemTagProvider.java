ab5e2e7c5facda04ec7b1d5c642e27f2acb872b1
AOA <aoa@d.co>
2025-04-23 16:08:33 +0300

    Fixing some bugs

---
+++ src/main/java/net/alminoris/almirisweapons/datagen/ModItemTagProvider.java
@@ -0,0 +1,36 @@
+package net.alminoris.almirisweapons.datagen;
+
+import net.alminoris.almirisweapons.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+
+import java.util.concurrent.CompletableFuture;
+
+import static net.alminoris.almirisweapons.util.helper.WeaponSetsHelper.MATERIALS;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider
+{
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture)
+    {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup)
+    {
+        for (String name : MATERIALS)
+        {
+            getOrCreateTagBuilder(ItemTags.SWORDS)
+                    .add(ModItems.HALBERDS.get(name))
+                    .add(ModItems.SAI.get(name))
+                    .add(ModItems.RAPIERS.get(name))
+                    .add(ModItems.CLAYMORES.get(name))
+                    .add(ModItems.MACES.get(name));
+
+            getOrCreateTagBuilder(ItemTags.AXES)
+                    .add(ModItems.BATTLE_AXES.get(name));
+        }
+    }
+}
