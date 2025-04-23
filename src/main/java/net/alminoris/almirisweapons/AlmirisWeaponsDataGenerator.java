57a41c99d7e70ca1ed787381b6bb99edc584940f
AOA <aoa@d.co>
2025-04-23 15:32:47 +0300

    Fixing some bugs

--- src/main/java/net/alminoris/almirisweapons/AlmirisWeaponsDataGenerator.java
+++ src/main/java/net/alminoris/almirisweapons/AlmirisWeaponsDataGenerator.java
@@ -1,5 +1,6 @@
 package net.alminoris.almirisweapons;
 
+import net.alminoris.almirisweapons.datagen.ModItemTagProvider;
 import net.alminoris.almirisweapons.datagen.ModModelProvider;
 import net.alminoris.almirisweapons.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
@@ -14,5 +15,6 @@ public class AlmirisWeaponsDataGenerator implements DataGeneratorEntrypoint
 
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
 	}
 }
