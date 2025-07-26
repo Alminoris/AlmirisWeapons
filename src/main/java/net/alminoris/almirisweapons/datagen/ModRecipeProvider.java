54507dafd4460458e885b30ddc8692f056267a7e
AOA <aoa@d.co>
2025-07-26 12:00:34 +0300

    #Update 2.0

--- src/main/java/net/alminoris/almirisweapons/datagen/ModRecipeProvider.java
+++ src/main/java/net/alminoris/almirisweapons/datagen/ModRecipeProvider.java
@@ -599,6 +599,76 @@ public class ModRecipeProvider extends FabricRecipeProvider
                 .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
                 .offerTo(recipeExporter);
 
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ODACHIS.get("wood"), 1)
+                .pattern("*  ")
+                .pattern(" # ")
+                .pattern("  /")
+                .input('*', ItemTags.PLANKS)
+                .input('#', ModItems.KATANAS.get("wood"))
+                .input('/', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(ModItems.KATANAS.get("wood")), conditionsFromItem(ModItems.KATANAS.get("wood")))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ODACHIS.get("stone"), 1)
+                .pattern("*  ")
+                .pattern(" # ")
+                .pattern("  /")
+                .input('*', ItemTags.STONE_TOOL_MATERIALS)
+                .input('#', ModItems.KATANAS.get("stone"))
+                .input('/', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(ModItems.KATANAS.get("stone")), conditionsFromItem(ModItems.KATANAS.get("stone")))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ODACHIS.get("iron"), 1)
+                .pattern("*  ")
+                .pattern(" # ")
+                .pattern("  /")
+                .input('*', Items.IRON_INGOT)
+                .input('#', ModItems.KATANAS.get("iron"))
+                .input('/', Items.STICK)
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(ModItems.KATANAS.get("iron")), conditionsFromItem(ModItems.KATANAS.get("iron")))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ODACHIS.get("gold"), 1)
+                .pattern("*  ")
+                .pattern(" # ")
+                .pattern("  /")
+                .input('*', Items.GOLD_INGOT)
+                .input('#', ModItems.KATANAS.get("gold"))
+                .input('/', Items.STICK)
+                .criterion(hasItem(Items.GOLD_INGOT), conditionsFromItem(Items.GOLD_INGOT))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(ModItems.KATANAS.get("gold")), conditionsFromItem(ModItems.KATANAS.get("gold")))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ODACHIS.get("diamond"), 1)
+                .pattern("*  ")
+                .pattern(" # ")
+                .pattern("  /")
+                .input('*', Items.DIAMOND)
+                .input('#', ModItems.KATANAS.get("diamond"))
+                .input('/', Items.STICK)
+                .criterion(hasItem(Items.DIAMOND), conditionsFromItem(Items.DIAMOND))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(ModItems.KATANAS.get("diamond")), conditionsFromItem(ModItems.KATANAS.get("diamond")))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ODACHIS.get("netherite"), 1)
+                .pattern("*  ")
+                .pattern(" # ")
+                .pattern("  /")
+                .input('*', Items.NETHERITE_INGOT)
+                .input('#', ModItems.KATANAS.get("netherite"))
+                .input('/', Items.STICK)
+                .criterion(hasItem(Items.NETHERITE_INGOT), conditionsFromItem(Items.NETHERITE_INGOT))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(ModItems.KATANAS.get("netherite")), conditionsFromItem(ModItems.KATANAS.get("netherite")))
+                .offerTo(recipeExporter);
+
         ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.MACES.get("wood"), 1)
                 .pattern(" ##")
                 .pattern(" ##")
